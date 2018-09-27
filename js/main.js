018a1791cdc0b1e0f49965e9f0da21dfe20a148b
giangntgg <42958745+giangntgg@users.noreply.github.com>
2018-09-27 20:12:56 +0700

    Update main.js
    Change from line 1 to line 151

--- js/main.js
+++ js/main.js
@@ -4,269 +4,25 @@
   "use strict";
 
   try {
-    //WidgetChart 1
-    var ctx = document.getElementById("widgetChart1");
-    if (ctx) {
-      ctx.height = 130;
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          type: 'line',
-          datasets: [{
-            data: [78, 81, 80, 45, 34, 12, 40],
-            label: 'Dataset',
-            backgroundColor: 'rgba(255,255,255,.1)',
-            borderColor: 'rgba(255,255,255,.55)',
-          },]
-        },
-        options: {
-          maintainAspectRatio: true,
-          legend: {
-            display: false
-          },
-          layout: {
-            padding: {
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0
-            }
-          },
-          responsive: true,
-          scales: {
-            xAxes: [{
-              gridLines: {
-                color: 'transparent',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                fontSize: 2,
-                fontColor: 'transparent'
-              }
-            }],
-            yAxes: [{
-              display: false,
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-          title: {
-            display: false,
-          },
-          elements: {
-            line: {
-              borderWidth: 0
-            },
-            point: {
-              radius: 0,
-              hitRadius: 10,
-              hoverRadius: 4
-            }
-          }
-        }
-      });
-    }
-
-
-    //WidgetChart 2
-    var ctx = document.getElementById("widgetChart2");
-    if (ctx) {
-      ctx.height = 130;
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-          type: 'line',
-          datasets: [{
-            data: [1, 18, 9, 17, 34, 22],
-            label: 'Dataset',
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(255,255,255,.55)',
-          },]
-        },
-        options: {
-
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          responsive: true,
-          tooltips: {
-            mode: 'index',
-            titleFontSize: 12,
-            titleFontColor: '#000',
-            bodyFontColor: '#000',
-            backgroundColor: '#fff',
-            titleFontFamily: 'Montserrat',
-            bodyFontFamily: 'Montserrat',
-            cornerRadius: 3,
-            intersect: false,
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                color: 'transparent',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                fontSize: 2,
-                fontColor: 'transparent'
-              }
-            }],
-            yAxes: [{
-              display: false,
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-          title: {
-            display: false,
-          },
-          elements: {
-            line: {
-              tension: 0.00001,
-              borderWidth: 1
-            },
-            point: {
-              radius: 4,
-              hitRadius: 10,
-              hoverRadius: 4
-            }
-          }
-        }
-      });
-    }
-
-
-    //WidgetChart 3
-    var ctx = document.getElementById("widgetChart3");
-    if (ctx) {
-      ctx.height = 130;
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-          type: 'line',
-          datasets: [{
-            data: [65, 59, 84, 84, 51, 55],
-            label: 'Dataset',
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(255,255,255,.55)',
-          },]
-        },
-        options: {
-
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          responsive: true,
-          tooltips: {
-            mode: 'index',
-            titleFontSize: 12,
-            titleFontColor: '#000',
-            bodyFontColor: '#000',
-            backgroundColor: '#fff',
-            titleFontFamily: 'Montserrat',
-            bodyFontFamily: 'Montserrat',
-            cornerRadius: 3,
-            intersect: false,
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                color: 'transparent',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                fontSize: 2,
-                fontColor: 'transparent'
-              }
-            }],
-            yAxes: [{
-              display: false,
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-          title: {
-            display: false,
-          },
-          elements: {
-            line: {
-              borderWidth: 1
-            },
-            point: {
-              radius: 4,
-              hitRadius: 10,
-              hoverRadius: 4
-            }
-          }
-        }
-      });
-    }
-
-
-    //WidgetChart 4
-    var ctx = document.getElementById("widgetChart4");
-    if (ctx) {
-      ctx.height = 115;
-      var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-          datasets: [
-            {
-              label: "My First dataset",
-              data: [78, 81, 80, 65, 58, 75, 60, 75, 65, 60, 60, 75],
-              borderColor: "transparent",
-              borderWidth: "0",
-              backgroundColor: "rgba(255,255,255,.3)"
-            }
-          ]
-        },
-        options: {
-          maintainAspectRatio: true,
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              display: false,
-              categoryPercentage: 1,
-              barPercentage: 0.65
-            }],
-            yAxes: [{
-              display: false
-            }]
-          }
-        }
-      });
-    }
-
     // Recent Report
-    const brandProduct = 'rgba(0,181,233,0.8)'
-    const brandService = 'rgba(0,173,95,0.8)'
+    const jc = 'rgba(0,181,233,0.8)'
+    const poly = 'rgba(0,173,95,0.8)'
 
-    var elements = 10
-    var data1 = [52, 60, 55, 50, 65, 80, 57, 70, 105, 115]
-    var data2 = [102, 70, 80, 100, 56, 53, 80, 75, 65, 90]
+    var elements = 4
+    var data1 = [67, 70, 80, 88]
+    var data2 = [69, 75, 79, 87]
 
-    var ctx = document.getElementById("recent-rep-chart");
+    var ctx = document.getElementById("recent-rep-chart-G");
     if (ctx) {
       ctx.height = 250;
       var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', ''],
+          labels: ['2015', '2016', '2017', '2018'],
           datasets: [
             {
-              label: 'My First dataset',
-              backgroundColor: brandService,
+              label: 'A level/IB degree holders',
+              backgroundColor: jc,
               borderColor: 'transparent',
               pointHoverBackgroundColor: '#fff',
               borderWidth: 0,
@@ -274,8 +30,8 @@
 
             },
             {
-              label: 'My Second dataset',
-              backgroundColor: brandProduct,
+              label: 'Diploma holders',
+              backgroundColor: poly,
               borderColor: 'transparent',
               pointHoverBackgroundColor: '#fff',
               borderWidth: 0,
@@ -306,7 +62,7 @@
                 beginAtZero: true,
                 maxTicksLimit: 5,
                 stepSize: 50,
-                max: 150,
+                max: 100,
                 fontFamily: "Poppins",
                 fontSize: 12
               },
@@ -330,9 +86,7 @@
         }
       });
     }
-
-    // Percent Chart
-    var ctx = document.getElementById("percent-chart");
+    var ctx = document.getElementById("percent-chart-G");
     if (ctx) {
       ctx.height = 280;
       var myChart = new Chart(ctx, {
@@ -341,27 +95,31 @@
           datasets: [
             {
               label: "My First dataset",
-              data: [60, 40],
+              data: [750, 550, 100],
               backgroundColor: [
                 '#00b5e9',
-                '#fa4251'
+                '#fa4251',
+                '#00ad5f'
               ],
               hoverBackgroundColor: [
                 '#00b5e9',
-                '#fa4251'
+                '#fa4251',
+                '#00ad5f'
               ],
               borderWidth: [
-                0, 0
+                0, 0, 0
               ],
               hoverBorderColor: [
+                'transparent',
                 'transparent',
                 'transparent'
               ]
             }
           ],
           labels: [
-            'Products',
-            'Services'
+            'A level/IB',
+            'Diploma',
+            'Others'
           ]
         },
         options: {
@@ -390,8 +148,9 @@
     console.log(error);
   }
 
-
-
+//EDIT END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//NO CHANGE BELOW
+  
   try {
 
     // Recent Report 2
